ab20608e177713c3849fe758654cad35fbc6a84c
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-17 02:40:43 +0900

    feat: K-Nearest Neighbors 모델 학습 후 예측

--- Ch01/01-3.py
+++ Ch01/01-3.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from fishes import *
 
-plt.scatter(bream_length, bream_weight)
-plt.scatter(smelt_length, smelt_weight)
-plt.xlabel('length')
-plt.ylabel('weight')
-plt.show()
+# plt.scatter(bream_length, bream_weight)
+# plt.scatter(smelt_length, smelt_weight)
+# plt.xlabel('length')
+# plt.ylabel('weight')
+# plt.show()
 
 length = bream_length + smelt_length
 weight = bream_weight + smelt_weight
@@ -16,5 +16,7 @@ fish_target = [1]*35 + [0]*14 # 1=도미, 0=빙어
 kn = KNeighborsClassifier()
 kn.fit(fish_data, fish_target)
 
-accuracy = kn.score(fish_data, fish_target)
-print(accuracy)
+# accuracy = kn.score(fish_data, fish_target)
+# print(accuracy)
+prediction = kn.predict([[30, 600], [10, 100]])
+print(prediction)
