1b65e05a5062e311e52136d958331033c6e14427
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-17 02:33:48 +0900

    feat: K-Nearest Neighbors 모델 사용
    - 길이 및 무게 데이터로 2차원 리스트 생성
    - 도미=positive, 빙어=negative로 정답 리스트 생성
    - K-최근접 이웃 모델에 학습 후 평가

--- Ch01/01-3.py
+++ Ch01/01-3.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
-from fish_data import *
+from sklearn.neighbors import KNeighborsClassifier
+from fishes import *
 
 plt.scatter(bream_length, bream_weight)
 plt.scatter(smelt_length, smelt_weight)
 plt.xlabel('length')
 plt.ylabel('weight')
 plt.show()
+
+length = bream_length + smelt_length
+weight = bream_weight + smelt_weight
+fish_data = [[l, w] for l, w in zip(length, weight)]
+fish_target = [1]*35 + [0]*14 # 1=도미, 0=빙어
+
+kn = KNeighborsClassifier()
+kn.fit(fish_data, fish_target)
+
+accuracy = kn.score(fish_data, fish_target)
+print(accuracy)
