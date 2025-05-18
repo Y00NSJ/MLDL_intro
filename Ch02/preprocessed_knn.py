a09a288bc2ab96753e65b3aa0f74dfcbc758de5e
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-19 02:04:14 +0900

    chore: Standardization 적용
    - 훈련 셋에 표준화 적용
    - 동일하게 표준화를 적용한 테스트 셋으로 평가 진행
    - 예측용 샘플 데이터 또한 표준화 적용해 검증

--- Ch02/preprocessed_knn.py
+++ Ch02/preprocessed_knn.py
@@ -11,16 +11,31 @@ fish_target = np.concatenate((np.ones(35), np.zeros(14)))
 train_input, test_input, train_target, test_target = (
     train_test_split(fish_data, fish_target, stratify=fish_target, random_state=42)) # 디폴트 25%
 
-kn = KNeighborsClassifier()
-kn.fit(train_input, train_target)
+# kn = KNeighborsClassifier()
+# kn.fit(train_input, train_target)
 
 sample = [[25, 150]]
-distances, indexes = kn.kneighbors(sample)
+# distances, indexes = kn.kneighbors(sample)
 
-plt.scatter(test_input[:, 0], test_input[:, 1])
-plt.scatter(25, 150, marker='^')
-plt.scatter(train_input[indexes, 0], train_input[indexes, 1], marker='D')
+### Standardization
+mean = np.mean(train_input, axis=0)
+std = np.std(train_input, axis=0)
+train_scaled = (train_input - mean) / std
+test_scaled = (test_input - mean) / std
 
+new_sample = (sample[0] - mean) / std
+
+kn = KNeighborsClassifier()
+kn.fit(train_scaled, train_target)
+
+kn.score(test_scaled, test_target)
+print(kn.predict([new_sample]))
+
+distances, indexes = kn.kneighbors([new_sample])
+
+plt.scatter(train_scaled[:, 0], train_scaled[:, 1])
+plt.scatter(new_sample[0], new_sample[1], marker='^')
+plt.scatter(train_scaled[indexes, 0], train_scaled[indexes, 1], marker='D')
 plt.xlabel('length')
 plt.ylabel('weight')
 plt.show()
