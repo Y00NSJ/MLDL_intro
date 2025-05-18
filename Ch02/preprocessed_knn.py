912458a885a524425b46407831fd86c1e7ec8320
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-19 01:49:19 +0900

    chore: 전처리의 필요성 이해

--- Ch02/preprocessed_knn.py
+++ Ch02/preprocessed_knn.py
@@ -1,6 +1,8 @@
 from fishes import *
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 
 
 fish_data = np.column_stack((fish_length, fish_weight))
@@ -8,3 +10,17 @@ fish_target = np.concatenate((np.ones(35), np.zeros(14)))
 
 train_input, test_input, train_target, test_target = (
     train_test_split(fish_data, fish_target, stratify=fish_target, random_state=42)) # 디폴트 25%
+
+kn = KNeighborsClassifier()
+kn.fit(train_input, train_target)
+
+sample = [[25, 150]]
+distances, indexes = kn.kneighbors(sample)
+
+plt.scatter(test_input[:, 0], test_input[:, 1])
+plt.scatter(25, 150, marker='^')
+plt.scatter(train_input[indexes, 0], train_input[indexes, 1], marker='D')
+
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
