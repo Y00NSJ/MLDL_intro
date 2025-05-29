c7632420945358b503a17a789ff7141a42cb8ed0
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-29 12:20:58 +0900

    feat: 훈련 데이터 / 테스트 데이터 분리
    - 타깃(농어 무게)은 기존 데이터 그대로 사용

--- Ch03/feature_engineer.py
+++ Ch03/feature_engineer.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 perch_full = pd.read_csv('https://bit.ly/perch_csv_data')   # 주소로부터 CSV 파일 읽어오기
-print(perch_full.head())                                           # 최초 5개 행 출력
+from perch_data import perch_weight
+
+train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42)
