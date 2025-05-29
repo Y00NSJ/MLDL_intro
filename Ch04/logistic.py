839ba6784def5c11d34fcd631efad76e38f5a16c
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 03:47:19 +0900

    feat: 훈련셋/테스트셋 분리 및 표준화 전처리
    - fish에서 '어종' 열을 선택해 타겟 데이터셋을 series 객체로 생성
    - train_test_split으로 테스트셋 분리
    - StandardScaler로 훈련셋, 테스트셋 표준화

--- Ch04/logistic.py
+++ Ch04/logistic.py
@@ -1,9 +1,17 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 fish = pd.read_csv('https://bit.ly/fish_csv_data')
-print(fish.columns.tolist()) # 특성 5개
-print("어종: ", pd.unique(fish['Species']))   # Species 열의 고유값 측정
+# print(fish.columns.tolist()) # 특성 5개
+# print("어종: ", pd.unique(fish['Species']))   # Species 열의 고유값 측정
 
 fish_input = fish[list(filter(lambda x: x != 'Species', fish))] # 어종을 제외한 5개 열을 입력 데이터로 사용
-print(fish_input.head())    # 새롭게 반환된 데이터프레임
+fish_target = fish['Species']   # Dataframe(X) Series 객체(O)
+
+
+train_input, test_input, train_target, test_target = train_test_split(fish_input, fish_target, random_state=42)
+ss = StandardScaler()
+ss.fit(train_input)
+train_input = ss.transform(train_input)
+test_input = ss.transform(test_input)
