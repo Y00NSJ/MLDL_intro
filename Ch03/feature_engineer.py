a7811be8290e9ba59bc29f6d923e0ac4b75b47b4
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-29 16:28:28 +0900

    feat: 전처리된 input 데이터를 통해 과소적합 이슈 해결
    - 훈련셋에 대한 점수 상승
    - 테스트셋에 대한 점수는 높아지지 않음

--- Ch03/feature_engineer.py
+++ Ch03/feature_engineer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 # 데이터 불러오고 split
 perch_full = pd.read_csv('https://bit.ly/perch_csv_data')   # 주소로부터 CSV 파일 읽어오기
@@ -11,5 +12,11 @@ train_input, test_input, train_target, test_target = train_test_split(perch_full
 poly = PolynomialFeatures()
 poly.fit(train_input)
 train_poly = poly.transform(train_input)
-print(poly.get_feature_names_out())
+# print(poly.get_feature_names_out())
 test_poly = poly.transform(test_input)  # info leak 예방 위해 훈련 세트에 적용한 변환기를 기준으로 테스트셋 변환
+
+# Multiple Linear Regression
+lr = LinearRegression()
+lr.fit(train_poly, train_target)
+print("Score of Train Set: ", lr.score(train_poly, train_target))
+print("Score of Test Set: ", lr.score(test_poly, test_target))
