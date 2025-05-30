0c8610ba783fd9d9bc463825eedbe3433fc46ba1
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 17:53:26 +0900

    feat: 훈련 점수 및 테스트 점수 산출
    - 83, 77로 과소적합 발생
    - 에포크 횟수 부족이 원인으로 추정

--- Ch04/StochasticGD.py
+++ Ch04/StochasticGD.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import SGDClassifier
 
 
 # 데이터 불러오기
-fish = pd.read_csv('https://bit.ly/fish_csv/data')
+fish = pd.read_csv('https://bit.ly/fish_csv_data')
 fish_input = fish[list(filter(lambda x : x != 'Species', fish))]
 fish_target = fish['Species']
 
@@ -21,3 +21,6 @@ test_scaled = ss.transform(test_input)
 # 확률적 경사 하강법을 통해 다중 분류 진행
 sc = SGDClassifier(loss='log_loss', max_iter=10, random_state=42)   # 손실 함수 종류=로지스틱 손실 함수, 에포크 횟수=10
 sc.fit(train_scaled, train_target)
+
+print("Training score:", sc.score(train_scaled, train_target))
+print("Test score:", sc.score(test_scaled, test_target))
