9b5dca4275bf717117dc2aebc1d639aeaa05fcfe
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 19:56:05 +0900

    feat: Decision Tree 모델 사용
    - 과대적합 발생

--- Ch05/Decision_Tree.py
+++ Ch05/Decision_Tree.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
 
 # read CSV
@@ -19,8 +20,14 @@ ss.fit(train_input)
 train_scaled = ss.transform(train_input)
 test_scaled = ss.transform(test_input)
 
-# Logistic Regression
-lr = LogisticRegression()
-lr.fit(train_scaled, train_target)
-print("train score: ", lr.score(train_scaled, train_target))
-print("test score: ", lr.score(test_scaled, test_target))
+### Logistic Regression
+# lr = LogisticRegression()
+# lr.fit(train_scaled, train_target)
+# print("train score: ", lr.score(train_scaled, train_target))
+# print("test score: ", lr.score(test_scaled, test_target))
+
+### Decision Tree
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(train_scaled, train_target)
+print("train score: ", dt.score(train_scaled, train_target))
+print("test score: ", dt.score(test_scaled, test_target))
