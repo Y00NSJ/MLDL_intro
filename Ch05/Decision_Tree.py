0b0c6fc77d94f36bee5a9edf434b780463e52e28
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 19:44:58 +0900

    docs: 단계별 주석 추가

--- Ch05/Decision_Tree.py
+++ Ch05/Decision_Tree.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
+# read CSV
 wine = pd.read_csv('https://bit.ly/wine_csv_data')
 data = wine[list(filter(lambda x : x != 'class', wine))]
 target = wine['class']
 
+# split train set / test set
 train_input, test_input, train_target, test_target = (
     train_test_split(data, target, test_size=0.2, random_state=42)) # 샘플 수가 충분히 많으므로 20%만 테스트셋으로 분리
+
