f10d401f1ec685930580bdc6d579794ce691f296
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 17:45:06 +0900

    feat: 데이터셋을 훈련 셋과 테스트 셋으로 분리
    - SKLearn의 train_test_split 메서드 사용

--- Ch04/StochasticGD.py
+++ Ch04/StochasticGD.py
@@ -1,6 +1,8 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 fish = pd.read_csv('https://bit.ly/fish_csv/data')
 fish_input = fish[list(filter(lambda x : x != 'Species', fish))]
 fish_target = fish['Species']
+
+train_input, test_input, train_target, test_target = train_test_split(fish_input, fish_target, random_state=42)
