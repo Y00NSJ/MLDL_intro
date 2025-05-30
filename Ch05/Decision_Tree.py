7605dfd30b955e7d698bb733887685872e503d41
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 19:43:25 +0900

    feat: 훈련셋 / 테스트셋 분리
    - 샘플 수가 충분히 많으므로 20%만 테스트셋으로 분리

--- Ch05/Decision_Tree.py
+++ Ch05/Decision_Tree.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 wine = pd.read_csv('https://bit.ly/wine_csv_data')
 data = wine[list(filter(lambda x : x != 'class', wine))]
 target = wine['class']
+
+train_input, test_input, train_target, test_target = (
+    train_test_split(data, target, test_size=0.2, random_state=42)) # 샘플 수가 충분히 많으므로 20%만 테스트셋으로 분리
