76211162559876ca6e5d559f576b5df7d9b54278
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-30 18:10:55 +0900

    feat: 도출해낸 최적 에포크 횟수로 훈련 진행
    - tol=None을 통해 성능 답보 시에도 조기 종료 금지
    - 96/92로 준수한 성능 도출

--- Ch04/StochasticGD.py
+++ Ch04/StochasticGD.py
@@ -21,29 +21,35 @@ train_scaled = ss.transform(train_input)
 test_scaled = ss.transform(test_input)
 
 ### 확률적 경사 하강법을 통해 다중 분류 진행
-sc = SGDClassifier(loss='log_loss', max_iter=10, random_state=42)   # 손실 함수 종류=로지스틱 손실 함수, 에포크 횟수=10
+# sc = SGDClassifier(loss='log_loss', max_iter=10, random_state=42)   # 손실 함수 종류=로지스틱 손실 함수, 에포크 횟수=10
+# sc.fit(train_scaled, train_target)
+# print("Training score:", sc.score(train_scaled, train_target))
+# print("Test score:", sc.score(test_scaled, test_target))
+#
+# # 앞선 모델에서, 이어서 훈련 진행
+# sc.partial_fit(train_scaled, train_target)  # 1에포크 추가 훈련
+# print("+ 1 epoch")
+# print("Training score:", sc.score(train_scaled, train_target))
+# print("Test score:", sc.score(test_scaled, test_target))
+
+# Accuracy for Epoch count
+# sc = SGDClassifier(loss='log_loss', random_state=42)
+# train_score = []
+# test_score = []
+# classes = np.unique(train_target)
+# for _ in range(0, 300):     # 300번 진행
+#     sc.partial_fit(train_scaled, train_target, classes=classes) # fit() 사용하지 않고 partial_fit()만 사용해 훈련, 클래스 레이블 전달
+#     train_score.append(sc.score(train_scaled, train_target))
+#     test_score.append(sc.score(test_scaled, test_target))
+
+# plt.plot(train_score)
+# plt.plot(test_score)
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.show()
+
+# 최적 에포크 횟수인 100으로 훈련 진행
+sc = SGDClassifier(loss='log_loss', max_iter=100, tol=None, random_state=42)    # 성능 답보 시에도 자동 얼리스탑 없이 강제 반복 설정
 sc.fit(train_scaled, train_target)
-print("Training score:", sc.score(train_scaled, train_target))
-print("Test score:", sc.score(test_scaled, test_target))
-
-# 앞선 모델에서, 이어서 훈련 진행
-sc.partial_fit(train_scaled, train_target)  # 1에포크 추가 훈련
-print("+ 1 epoch")
-print("Training score:", sc.score(train_scaled, train_target))
-print("Test score:", sc.score(test_scaled, test_target))
-
-# Early Stopping
-sc = SGDClassifier(loss='log_loss', random_state=42)
-train_score = []
-test_score = []
-classes = np.unique(train_target)
-for _ in range(0, 300):     # 300번 진행
-    sc.partial_fit(train_scaled, train_target, classes=classes) # fit() 사용하지 않고 partial_fit()만 사용해 훈련, 클래스 레이블 전달
-    train_score.append(sc.score(train_scaled, train_target))
-    test_score.append(sc.score(test_scaled, test_target))
-
-plt.plot(train_score)
-plt.plot(test_score)
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.show()
+print("Training score of 100 epoch:", sc.score(train_scaled, train_target))
+print("Test score of 100 epoch:", sc.score(test_scaled, test_target))
