a0feec77a30efa88862b8391baf38d6b428e53fc
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-05-31 18:46:33 +0900

    feat: train set / test set 분리

--- Ch05/Validation_Set.py
+++ Ch05/Validation_Set.py
@@ -1,6 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
+# read CSV
 wine = pd.read_csv('https://bit.ly/wine_csv_data')
 data = wine[list(filter(lambda x : x != 'class', wine))]
 target = wine['class']
+
+# split train set / test set
+train_input, test_input, train_target, test_target = (
+    train_test_split(data, target, test_size=0.2, random_state=42))
