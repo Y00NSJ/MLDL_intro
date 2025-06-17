4c3201e459ed4ed71308b95c1c7e5bc192c77de7
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-17 23:24:07 +0900

    feat: 패션 MNIST 중 10개 샘플을 이미지로 확인
    - plt.imshow 사용

--- Ch07/artificial_neural_network.py
+++ Ch07/artificial_neural_network.py
@@ -1,7 +1,12 @@
 import keras
+import matplotlib.pyplot as plt
 
 
 # fetch Fashion MNIST dataset
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
-print("shapes:", train_input.shape, train_target.shape)
-print("shapes:", test_input.shape, test_target.shape)
+fig, axs = plt.subplots(1, 10, figsize=(10, 10))    # 1개 행, 10개 열
+for i in range(10):
+    axs[i].imshow(train_input[i], cmap='gray_r')
+    axs[i].axis('off')
+plt.show()
+print(train_target[:10])
