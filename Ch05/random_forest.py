839622cecdc2584e57262e3cd576940db871b344
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-17 21:54:58 +0900

    feat: Extra Tree 훈련
    - 랜덤 포레스트와 비슷한 결과 획득

--- Ch05/random_forest.py
+++ Ch05/random_forest.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
 
 ### read CSV
@@ -21,6 +21,11 @@ train_input, test_input, train_target, test_target = train_test_split(data, targ
 # print("feature importance:", rf.feature_importances_)   # 하나의 특성에 과집중 (X), 좀 더 다양한 특성이 훈련에 기여 => 일반화 성능 향상
 
 ### use OOB sample
-rf = RandomForestClassifier(oob_score=True, n_jobs=-1, random_state=42)
-rf.fit(train_input, train_target)
-print("Evaluation Score using OOB:", rf.oob_score_)
+# rf = RandomForestClassifier(oob_score=True, n_jobs=-1, random_state=42)
+# rf.fit(train_input, train_target)
+# print("Evaluation Score using OOB:", rf.oob_score_)
+
+### Extra Trees
+et = ExtraTreesClassifier(n_jobs=-1, random_state=42)
+scores = cross_validate(et, train_input, train_target, return_train_score=True, n_jobs=-1)
+print(f"train score: {np.mean(scores['train_score'])}\ntest score: {np.mean(scores['test_score'])}")
