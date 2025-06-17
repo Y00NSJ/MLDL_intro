36d23c092415e429fd6250d6f3141049a11b125d
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-17 21:46:26 +0900

    fix: 교차 검증 대신 OOB score 활용해 평가
    - 검증 셋의 역할을 하므로 교차 검증과 유사한 기능
    - 훈련 셋에 더 많은 샘플 사용 가능

--- Ch05/random_forest.py
+++ Ch05/random_forest.py
@@ -4,18 +4,23 @@ from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.ensemble import RandomForestClassifier
 
 
-# read CSV
+### read CSV
 wine = pd.read_csv('https://bit.ly/wine_csv_data')
 data = wine[list(filter(lambda x : x != 'class', wine))]
 target = wine['class']
 
-# split between train set and test set
+### split between train set and test set
 train_input, test_input, train_target, test_target = train_test_split(data, target, test_size=0.2, random_state=42)
 
-# train Random Forest and cross validate
-rf = RandomForestClassifier(n_jobs=-1, random_state=42)
+### train Random Forest and cross validate
+# rf = RandomForestClassifier(n_jobs=-1, random_state=42)
 # scores = cross_validate(rf, train_input, train_target, return_train_score=True, n_jobs=-1)
 # print(f"train score: {np.mean(scores['train_score'])}\ntest score: {np.mean(scores['test_score'])}")  # overfitting
 
+# rf.fit(train_input, train_target)
+# print("feature importance:", rf.feature_importances_)   # 하나의 특성에 과집중 (X), 좀 더 다양한 특성이 훈련에 기여 => 일반화 성능 향상
+
+### use OOB sample
+rf = RandomForestClassifier(oob_score=True, n_jobs=-1, random_state=42)
 rf.fit(train_input, train_target)
-print("feature importance:", rf.feature_importances_)   # 하나의 특성에 과집중 (X), 좀 더 다양한 특성이 훈련에 기여 => 일반화 성능 향상
+print("Evaluation Score using OOB: ", rf.oob_score_)
