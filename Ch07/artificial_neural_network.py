79c308016ed06d9b7744325403f61a8d1ad7e0a9
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-17 23:25:31 +0900

    feat: 패션 MNIST의 레이블 별 샘플 개수 확인
    - 0~9 레이블별 6,000개 샘플 존재

--- Ch07/artificial_neural_network.py
+++ Ch07/artificial_neural_network.py
@@ -1,12 +1,14 @@
 import keras
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # fetch Fashion MNIST dataset
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 fig, axs = plt.subplots(1, 10, figsize=(10, 10))    # 1개 행, 10개 열
-for i in range(10):
-    axs[i].imshow(train_input[i], cmap='gray_r')
-    axs[i].axis('off')
-plt.show()
-print(train_target[:10])
+# for i in range(10):
+#     axs[i].imshow(train_input[i], cmap='gray_r')
+#     axs[i].axis('off')
+# plt.show()
+# print(train_target[:10])
+print(np.unique(train_target, return_counts=True))
