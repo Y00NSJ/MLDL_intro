2c640526f90e0c5c7d3fadc65d2c5862eb914d75
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-17 21:34:22 +0900

    feat: 랜덤 포레스트 훈련 및 교차 검증
    - Random Forest: 모든 CPU 코어 사용
    - Cross Validation: 최대한 병렬로 교차 검증
      - `return_train_score`로 훈련 셋에 대한 점수를 함께 반환 => 검증 셋 점수와 비교해 과대적합여부 파악 목적

--- Ch05/random_forest.py
+++ Ch05/random_forest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.ensemble import RandomForestClassifier
 
 
 # read CSV
@@ -10,3 +11,8 @@ target = wine['class']
 
 # split between train set and test set
 train_input, test_input, train_target, test_target = train_test_split(data, target, test_size=0.2, random_state=42)
+
+# train Random Forest and cross validate
+rf = RandomForestClassifier(n_jobs=-1, random_state=42)
+scores = cross_validate(rf, train_input, train_target, return_train_score=True, n_jobs=-1)
+print(f"train score: {np.mean(scores['train_score'])}\ntest score: {np.mean(scores['test_score'])}")
