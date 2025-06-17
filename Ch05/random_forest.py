fix: Gradient Boosting의 특성 중요도 관찰
- 랜덤 포레스트보다 특정 특성에 집중하는 편

Ch05/random_forest.py
@@ -32,6 +32,8 @@ train_input, test_input, train_target, test_target = train_test_split(data, targ
 # print("feature importance:", et.feature_importances_)
 
 ### Gradient Boosting
-gb = GradientBoostingClassifier(n_estimators=500, learning_rate=0.2, random_state=42)
-scores = cross_validate(gb, train_input, train_target, return_train_score=True, n_jobs=-1)
-print(f"train score: {np.mean(scores['train_score'])}\ntest score: {np.mean(scores['test_score'])}")
+gb = GradientBoostingClassifier(random_state=42)
+# scores = cross_validate(gb, train_input, train_target, return_train_score=True, n_jobs=-1)
+# print(f"train score: {np.mean(scores['train_score'])}\ntest score: {np.mean(scores['test_score'])}")
+gb.fit(train_input, train_target)
+print("feature importance:", gb.feature_importances_)