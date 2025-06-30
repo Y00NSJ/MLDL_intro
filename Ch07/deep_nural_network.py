411296ee7e3adab1d4671ed67c1dcf9b65779bdf
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-30 16:59:36 +0900

    feat: 신경망 층 구성

--- Ch07/deep_nural_network.py
+++ Ch07/deep_nural_network.py
@@ -1,7 +1,19 @@
 import keras
 from sklearn.model_selection import train_test_split
 
+
+### 전처리
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 train_scaled = train_input / 255.0
 train_scaled = train_scaled.reshape(-1, 28*28)
 train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+
+
+### 입력층 정의
+inputs = keras.layers.Input(shape=(784,))
+
+### 밀집층 정의
+# 은닉층: sigmoid 사용
+dense1 = keras.layers.Dense(100, activation='sigmoid')
+# 출력층: softmax 사용
+dense2 = keras.layers.Dense(10, activation='softmax')
