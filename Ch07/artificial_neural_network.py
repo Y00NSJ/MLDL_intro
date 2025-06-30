da722b64d3bdfc29c3ce322ed51041c0f6ee0742
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-06-30 11:17:42 +0900

    feat: 로지스틱 회귀 및 교차 검증
    - max_iter를 5에서 50까지 올렸으나 성능이 크게 향상되지 않음

--- Ch07/artificial_neural_network.py
+++ Ch07/artificial_neural_network.py
@@ -1,7 +1,8 @@
 import keras
 import matplotlib.pyplot as plt
 import numpy as np
-
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import SGDClassifier
 
 # fetch Fashion MNIST dataset
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
@@ -17,4 +18,8 @@ import numpy as np
 # SGD Classifier를 사용하기 위해 각 이미지 샘플을 1차원으로 reshape
 train_scaled = train_input / 255.0
 train_scaled = train_scaled.reshape(-1, 28*28)  # 784
-print(f"train_input shape: {train_input.shape}\ntrain_scaled shape: {train_scaled.shape}")
+
+# 로지스틱 회귀 및 교차 검증
+sc = SGDClassifier(loss='log_loss', max_iter=50, random_state=42)
+scores = cross_validate(sc, train_scaled, train_target, n_jobs=-1)
+print(f"average test score: {np.mean(scores['test_score'])}")
