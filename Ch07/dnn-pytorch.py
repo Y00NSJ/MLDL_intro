e875480f98f499e5da66d9cb0f7f6884a6f9d0a8
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-22 21:42:39 +0900

    docs: 받아온 데이터의 속성 확인

--- Ch07/dnn-pytorch.py
+++ Ch07/dnn-pytorch.py
@@ -1,5 +1,7 @@
 from torchvision.datasets import FashionMNIST
 
 
-fm_train = FashionMNIST(root='.', train=True, download=True)
+fm_train = FashionMNIST(root='.', train=True, download=True)    # 저장될 위치, 훈련/테스트 선택, 다운로드해 로컬에 저장
 fm_test = FashionMNIST(root='.', train=False, download=True)
+type(fm_train.data) # 데이터는 객체의 data 속성에 PyTorch Tensor로 저장됨
+# Tensor: PyTorch의 기본 데이터 구조
