1f75fd4551b65b2e0af518be6f2d6fd6e130f525
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-22 21:30:08 +0900

    refactor: 옵티마이저로 Adagrad 사용

--- Ch07/deep_neural_network.py
+++ Ch07/deep_neural_network.py
@@ -38,8 +38,9 @@ model.summary()
 ### 생성한 DNN 훈련
 # sgd = keras.optimizers.SGD()    # 기본적인 확률적 경사하강법
 # sgd = keras.optimizers.SGD(learning_rate=0.1)   # 학습률 지정
-sgd = keras.optimizers.SGD(momentum=0.9, nesterov=True) # 네스테로프 모멘텀 최적화
-model.compile(optimizer=sgd, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# sgd = keras.optimizers.SGD(momentum=0.9, nesterov=True) # 네스테로프 모멘텀 최적화
+adagrad = keras.optimizers.Adagrad()
+model.compile(optimizer=adagrad, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(train_scaled, train_target, epochs=5)
 
 # 검증 세트에서의 성능 확인
