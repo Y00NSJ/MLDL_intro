61414f090e4bb37341f2cf32a6a7ff5433053a4b
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-22 21:45:52 +0900

    feat: 훈련 셋에서 검증 셋 분리

--- Ch07/dnn-pytorch.py
+++ Ch07/dnn-pytorch.py
@@ -1,4 +1,5 @@
 from torchvision.datasets import FashionMNIST
+from sklearn.model_selection import train_test_split
 
 
 fm_train = FashionMNIST(root='.', train=True, download=True)    # 저장될 위치, 훈련/테스트 선택, 다운로드해 로컬에 저장
@@ -9,3 +10,4 @@ print(fm_train.targets.shape, fm_test.targets.shape)  # 타깃은 1차원 배열
 train_input = fm_train.data
 train_target = fm_train.targets
 train_scaled = train_input / 255.0  # 입력 정규화
+train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
