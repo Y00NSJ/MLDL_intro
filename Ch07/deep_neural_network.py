0573e9a745f65e8e9262db2f166412adf741b4c2
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-22 21:12:57 +0900

    fix: flatten layer를 추가했으므로 reshape 전처리 과정 삭제

--- Ch07/deep_neural_network.py
+++ Ch07/deep_neural_network.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 ### 전처리
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 train_scaled = train_input / 255.0  # 픽셀값을 0~255 범위에서 0~1 사이로 정규화
-train_scaled = train_scaled.reshape(-1, 28*28)  # 2차원 배열을 1차원 배열로 펼침
+# train_scaled = train_scaled.reshape(-1, 28*28)  # 2차원 배열을 1차원 배열로 펼침
 train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
 
 
