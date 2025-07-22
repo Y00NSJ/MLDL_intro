33a8299c4dbd1b0c4000787e2396d45498059e67
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-22 21:48:54 +0900

    feat: PyTorch 모델 생성

--- Ch07/dnn-pytorch.py
+++ Ch07/dnn-pytorch.py
@@ -1,5 +1,6 @@
 from torchvision.datasets import FashionMNIST
 from sklearn.model_selection import train_test_split
+import torch.nn as nn
 
 
 ### 데이터 다운로드
@@ -15,3 +16,9 @@ train_scaled = train_input / 255.0  # 입력 정규화
 train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
 
 ### 모델 생성
+model = nn.Sequential(                          # 케라스와 달리 모델 입력 크기 사전 지정 X
+    nn.Flatten(),
+    nn.Linear(784, 100),  # 케라스의 Dense 층; 매개변수로 입력 크기와 출력 크기(뉴런 개수) 전달
+    nn.ReLU(),                                  # 활성 함수는 별도 층으로 추가
+    nn.Linear(100, 10)    # 출력층의 활성함수 생략
+)
