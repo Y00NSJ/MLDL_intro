243d95fd1ab38e5586c494d032f17e050d3943ac
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-23 23:58:07 +0900

    refactor: 에포크 횟수 20으로 증가

--- Ch07/model_train.py
+++ Ch07/model_train.py
@@ -24,8 +24,8 @@ model.summary()
 
 # 모델 훈련 후 결과 history 객체 확인
 model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(train_scaled, train_target, epochs=5, verbose=0)
-print(history.history.keys())
+history = model.fit(train_scaled, train_target, epochs=20, verbose=0)
+# print(history.history.keys())
 # 에포크 별 손실 도식화
 plt.plot(history.history['loss'])
 plt.xlabel('epoch')
