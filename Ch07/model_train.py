7a8d4e7993f1dd763743dcbab93ede74cf85b816
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-24 00:14:14 +0900

    feat: 모델 저장 - 필요한 정보를 압축해 `.keras` 확장자로 저장

--- Ch07/model_train.py
+++ Ch07/model_train.py
@@ -18,27 +18,27 @@ def model_fn(a_layer=None):
     model.add(keras.layers.Dense(10, activation='softmax'))
     return model
 
-## 3개 층 모델 생성
-model = model_fn()
-model.summary()
-
-# 모델 훈련 후 결과 history 객체 확인
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(train_scaled, train_target, epochs=20, verbose=0, validation_data=(val_scaled, val_target))
-# print(history.history.keys())
-# 에포크 별 손실 도식화
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='val')
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.legend()
-plt.show()
-# 에포크 별 정확도 도식화
-plt.plot(history.history['accuracy'])
-plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.legend()
-plt.show()
+# ## 3개 층 모델 생성
+# model = model_fn()
+# model.summary()
+#
+# # 모델 훈련 후 결과 history 객체 확인
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# history = model.fit(train_scaled, train_target, epochs=20, verbose=0, validation_data=(val_scaled, val_target))
+# # print(history.history.keys())
+# # 에포크 별 손실 도식화
+# plt.plot(history.history['loss'], label='train')
+# plt.plot(history.history['val_loss'], label='val')
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.legend()
+# plt.show()
+# # 에포크 별 정확도 도식화
+# plt.plot(history.history['accuracy'])
+# plt.xlabel('epoch')
+# plt.ylabel('accuracy')
+# plt.legend()
+# plt.show()
 
 ### 드롭아웃 층 추가
 model = model_fn(keras.layers.Dropout(0.3))
@@ -54,3 +54,6 @@ plt.xlabel('epoch')
 plt.ylabel('loss')
 plt.legend()
 plt.show()
+
+# 모델 저장
+model.save('model-whole.keras')
