9eaa58a8f6e0479a17b26b5f51aaca7eaef78671
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-23 23:21:47 +0900

    feat: 모델 생성 함수 정의
    - 매개변수 `a_layer`: 객체 생성 시 layer 인자 주입하면 은닉층 뒤에 층 추가

---
+++ Ch07/model_train.py
@@ -0,0 +1,20 @@
+import keras
+from sklearn.model_selection import train_test_split
+
+
+### 데이터 전처리
+(train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
+train_scaled = train_input / 255.0
+train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+
+### 모델 생성 함수 정의
+def model_fn(a_layer=None):
+    model = keras.Sequential()
+    model.add(keras.layers.Input(shape=(28, 28)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(100, activation='relu'))
+    if a_layer:
+        model.add(a_layer)
+    model.add(keras.layers.Dense(10, activation='softmax'))
+    return model
+
