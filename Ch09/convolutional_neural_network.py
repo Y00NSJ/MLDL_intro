33054795d0844b6d9143afe710124e1258daa66c
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-25 02:35:10 +0900

    feat: 합성곱 층에 same padding 적용

--- Ch09/convolutional_neural_network.py
+++ Ch09/convolutional_neural_network.py
@@ -2,4 +2,4 @@ import keras
 
 
 ## 2차원 입력의 왼쪽 위에서 오른쪽 아래로 이동하는 합성곱 층
-keras.layers.Conv2D(10, kernal_size=(3, 3), activation='relu')  # 필터 개수, 커널 크기, 활성 함수
+keras.layers.Conv2D(10, kernal_size=(3, 3), activation='relu', padding='same')  # 필터 개수, 커널 크기, 활성 함수, same padding
