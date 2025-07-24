ad0e5194c7125c4c11e04f9f9cf5e0419896abd8
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-25 02:45:58 +0900

    feat: 풀링층 객체 생성
    - 크기는 2로 지정

--- Ch09/convolutional_neural_network.py
+++ Ch09/convolutional_neural_network.py
@@ -3,3 +3,5 @@ import keras
 
 ## 2차원 입력의 왼쪽 위에서 오른쪽 아래로 이동하는 합성곱 층
 keras.layers.Conv2D(10, kernal_size=(3, 3), activation='relu', padding='same', strides=1)  # 필터 개수, 커널 크기
+## Max Pooling 수행
+keras.layers.MaxPooling2D(2) # 풀링 크기
