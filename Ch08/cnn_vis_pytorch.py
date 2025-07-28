cb46d28d60ba39d684724ba6cfbe7e47d8124b18
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-07-28 17:45:16 +0900

    feat: 이전 모델 로드
    - 동일한 모델 구성 후 가중치 로드

---
+++ Ch08/cnn_vis_pytorch.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch
+import matplotlib.pyplot as plt
+from torchvision.datasets import FashionMNIST
+
+from Ch08.cnn_visualization import ankle_boot
+
+### 동일한 모델 생성 후 가중치 로드
+model = nn.Sequential()
+model.add_module('conv1', nn.Conv2d(1, 32, kernel_size=3, padding='same'))
+model.add_module('relu1', nn.ReLU())
+model.add_module('pool1', nn.MaxPool2d(2))
+model.add_module('conv2', nn.Conv2d(32, 64, kernel_size=3, padding='same'))
+model.add_module('relu2', nn.ReLU())
+model.add_module('pool2', nn.MaxPool2d(2))
+model.add_module('flatten', nn.Flatten())
+model.add_module('dense1', nn.Linear(3136, 100))
+model.add_module('relu3', nn.ReLU())
+model.add_module('dropout', nn.Dropout(0.3))
+model.add_module('dense2', nn.Linear(100, 10))
+# 가중치 로드
+model.load_state_dict(torch.load('best_cnn_model.pt', weights_only=True))
+
