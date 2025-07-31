36a527f737c5b4c198b4c002e52bb68d7557e1dd
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-08-01 01:36:39 +0900

    test: 훈련 셋의 0번 리뷰로 리뷰의 구조 파악 - n개 토큰만큼의 크기의 리스트로 구성 - 리스트는 정수로 변환된 각 토큰으로 구성   - 어휘 사전에 등재된 200개 단어 외엔 전부 모르는 단어이므로 2로 표시

--- Ch09/recurrent_neural_network.py
+++ Ch09/recurrent_neural_network.py
@@ -5,3 +5,6 @@ from keras.datasets import imdb
 # 전체 데이터셋에서 가장 자주 등장하는 단어 200개만 사용
 # 텍스트의 길이 제각각 => 비정형 2차원 배열 => 각 리뷰마다 별도의 파이썬 리스트 사용 => 1차원 배열
 (train_input, train_target), (test_input, test_target) = imdb.load_data(num_words=200)  # (25000, ) : (25000, )
+# 첫 번째 리뷰
+print("첫 번째 리뷰의 길이: ", len(train_input[0]))
+print("첫 번째 리뷰의 내용:\n", train_input[0])
