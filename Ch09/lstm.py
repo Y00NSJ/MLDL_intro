f209886c1f8847d9a4c6b42d73068684e5865e8c
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-08-04 12:28:11 +0900

    feat: LSTM 순환층 모델 구성

--- Ch09/lstm.py
+++ Ch09/lstm.py
@@ -1,9 +1,18 @@
 from keras.datasets import imdb
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.sequence import pad_sequences
+import keras
+
 
 (train_input, train_target), (test_input, test_target) = imdb.load_data(num_words=500)
 train_input, val_input, train_target, val_target = train_test_split(train_input, train_target, test_size=0.2, random_state=42)
 
 train_seq = pad_sequences(train_input, maxlen=100)
 val_seq = pad_sequences(val_input, maxlen=100)
+
+## LSTM 순환층
+model_lstm = keras.Sequential()
+model_lstm.add(keras.layers.Input(shape=(100,)))
+model_lstm.add(keras.layers.Embedding(500, 16))
+model_lstm.add(keras.layers.LSTM(8))
+model_lstm.add(keras.layers.Dense(1, activation='sigmoid'))
